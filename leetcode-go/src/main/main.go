031b26170382f2eaac7ad9b1f8729bbdd3ed4e5c
”Bi11ions” <roxwang@foxmail.com>
2021-09-23 16:06:52 +0800

    git feat: 386 字典排序

--- leetcode-go/src/main/main.go
+++ leetcode-go/src/main/main.go
@@ -17,7 +17,8 @@ func main() {
 	// fmt.Println(FindKthInList(headA, 3))
 	// fmt.Println(MaxSubArray([]int{1, 2, -1, 2, 4, -2}))
 	// fmt.Println(Trap([]int{4, 2, 0, 3, 2, 5}))
-	fmt.Println(IsValid("(){}}{"))
+	//fmt.Println(IsValid("(){}}{"))
+	fmt.Println(LexicalOrder(13))
 }
 
 /**
