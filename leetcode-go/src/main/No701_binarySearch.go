ca130db261a301a5f62c4de7b2553d860cea28b8
”Bi11ions” <roxwang@foxmail.com>
2021-09-28 18:15:30 +0800

    feat:字符串相加

---
+++ leetcode-go/src/main/No701_binarySearch.go
@@ -0,0 +1,24 @@
+package main
+
+// binarySearch 二分查找 如果目标值存在返回下标，否则返回 -1。
+func binarySearch(nums []int, target int) int {
+	return searchInBinary(nums, 0, len(nums)-1, target)
+}
+
+func searchInBinary(nums []int, startIndex, endIndex, target int) int {
+	if startIndex >= endIndex {
+		if target == nums[startIndex] {
+			return startIndex
+		}
+
+		return -1
+	}
+
+	// 防止数值溢出
+	mid := startIndex + (endIndex-startIndex)/2
+	if nums[mid] < target {
+		return searchInBinary(nums, mid+1, endIndex, target)
+	}
+
+	return searchInBinary(nums, startIndex, mid, target)
+}
